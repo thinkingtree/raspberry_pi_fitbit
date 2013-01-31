b7277410473936bb7df0c2bb1a6d9e3ddafab286
Justin Schumacher <justin@sweetspotdiabetes.com>
2013-01-30 22:39:35 -0800

    Added INIT info

--- fitbit_control.rb
+++ fitbit_control.rb
@@ -3,4 +3,14 @@
 require 'rubygems'
 require 'daemons'
 
+### BEGIN INIT INFO
+# Provides:          fitbit
+# Required-Start:    $remote_fs $syslog $network $named
+# Required-Stop:     $remote_fs $syslog $network $named
+# Default-Start:     3 4 5
+# Default-Stop:      0 1 2 6
+# Short-Description: Start fitbit service at boot.
+# Description:       Enable fitbit service.
+### END INIT INFO
+
 Daemons.run('fitbit.rb')
